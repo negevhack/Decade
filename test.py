4d56475494b8b33961f3838ffa0a771d23408a1b
negevhack <negevhack@gmail.com>
2021-12-03 19:47:30 +0800

    aye

---
+++ test.py
@@ -0,0 +1,10 @@
+def jumlahkan(a,b):
+    jum=a+b
+    print(a, "+", b,"=", jum)
+
+pilih="y"
+
+while(pilih=="y" or pilih=="Y"):
+    
+    jumlahkan(12,7)
+    pilih=input("Anda ingin menghitung lagi (y/n): ")
